12cb4be83db8105845a107a78dd2f3591ec6c689
Mike Causer <mcauser@gmail.com>
2014-10-01 00:21:53 +1000

    I2C revision for Model B+ added

--- Software/Python/grove_i2c_barometic_sensor_example.py
+++ Software/Python/grove_i2c_barometic_sensor_example.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import smbus
+import RPi.GPIO as GPIO
 #import grovepi
 from grove_i2c_barometic_sensor import BMP085
 
@@ -18,7 +19,11 @@ bmp = BMP085(0x77, 1)
 # bmp = BMP085(0x77, 2)  # HIRES Mode
 # bmp = BMP085(0x77, 3)  # ULTRAHIRES Mode
 
-bus = smbus.SMBus(1)
+rev = GPIO.RPI_REVISION
+if rev == 2 or rev == 3:
+    bus = smbus.SMBus(1)
+else:
+    bus = smbus.SMBus(0)
 
 temp = bmp.readTemperature()
 
