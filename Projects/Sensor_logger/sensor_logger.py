7177e1e54a9b9ca36ad1dcf203d9387861bcf749
Jan93 <jan.zenkner@gmail.com>
2014-11-03 17:42:26 +0100

    Minor Code improvements

--- Projects/Sensor_logger/sensor_logger.py
+++ Projects/Sensor_logger/sensor_logger.py
@@ -67,7 +67,10 @@ def writeAverage(log_file):
             log_file.write("HOUR: %d MIN / MAX TEMPS: ||| IN: %.2f / %.2f ||| OUT: %.2f / %.2f \n" %(current_hour,temp_in_min,temp_in_max,temp_out_min,temp_out_max)) 
             init()
 
+
+
 init()
+
 while True:
         try:
 		[temp,humidity] = grovepi.dht(temperature_sensor_in,0)
