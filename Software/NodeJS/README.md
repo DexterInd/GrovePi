593365566d3957cf8b911778fe4274c368839988
Marcello Barile <marcello.barile@gmail.com>
2015-02-05 00:50:25 +0100

    first commit

---
+++ Software/NodeJS/README.md
@@ -0,0 +1,8 @@
+GrovePi for Node.js
+=======
+
+GrovePi is an open source platform for connecting Grove Sensors to the Raspberry Pi.
+
+These files have been made available online through a [Creative Commons Attribution-ShareAlike 3.0](http://creativecommons.org/licenses/by-sa/3.0/) license.
+
+Started 2015/02/05
---
+++ Software/NodeJS/grovepi.commands.js
@@ -0,0 +1,64 @@
+module.exports = {
+  //Command Format
+  // digitalRead() command format header
+    dRead                                       : [1]
+  // digitalWrite() command format header
+  , dWrite                                      : [2]
+  // analogRead() command format header
+  , aRead                                       : [3]
+  // analogWrite() command format header
+  , aWrite                                      : [4]
+  // pinMode() command format header
+  , pMode                                       : [5]
+  // Ultrasonic read
+  , uRead                                       : [7]
+  // Get firmware version
+  , version                                     : [8]
+  // Accelerometer (+/- 1.5g) read
+  , acc_xyz                                     : [20]
+  // RTC get time
+  , rtc_getTime                                 : [30]
+  // DHT Pro sensor temperature
+  , dht_temp                                    : [40]
+
+  // Grove LED Bar commands
+  // Initialise
+  , ledBarInit                                  : [50]
+  // Set orientation
+  , ledBarOrient                                : [51]
+  // Set level
+  , ledBarLevel                                 : [52]
+  // Set single LED
+  , ledBarSetOne                                : [53]
+  // Toggle single LED
+  , ledBarToggleOne                             : [54]
+  // Set all LEDs
+  , ledBarSet                                   : [55]
+  // Get current state
+  , ledBarGet                                   : [56]
+
+  // Grove 4 Digit Display commands
+  // Initialise
+  , fourDigitInit                               : [70]
+  // Set brightness, not visible until next cmd
+  , fourDigitBrightness                         : [71]
+  // Set numeric value without leading zeros
+  , fourDigitValue                              : [72]
+  // Set numeric value with leading zeros
+  , fourDigitValueZeros                         : [73]
+  // Set individual digit
+  , fourDigitIndividualDigit                    : [74]
+  // Set individual leds of a segment
+  , fourDigitIndividualLeds                     : [75]
+  // Set left and right values with colon
+  , fourDigitScore                              : [76]
+  // Analog read for n seconds
+  , fourDigitAnalogRead                         : [77]
+  // Entire display on
+  , fourDigitAllOn                              : [78]
+  // Entire display off
+  , fourDigitAllOff                             : [79]
+
+  // This allows us to be more specific about which commands contain unused bytes
+  , unused                                      : 0
+};
