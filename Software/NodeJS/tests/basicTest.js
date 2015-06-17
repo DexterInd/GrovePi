1984ea20db17e405e7dafe42d86af2d5bd0ac8f5
Marcello Barile <marcello.barile@gmail.com>
2015-06-17 21:26:09 +0200

    Added light sensor + tweaks

--- Software/NodeJS/tests/basicTest.js
+++ Software/NodeJS/tests/basicTest.js
@@ -5,6 +5,7 @@ var AccelerationI2cSensor = GrovePi.sensors.AccelerationI2C
 var UltrasonicDigitalSensor = GrovePi.sensors.UltrasonicDigital
 var AirQualityAnalogSensor = GrovePi.sensors.AirQualityAnalog
 var DHTDigitalSensor = GrovePi.sensors.DHTDigital
+var LightAnalogSensor = GrovePi.sensors.LightAnalog
 
 var board
 
@@ -27,41 +28,52 @@ function start() {
         // Analog Port 1
         var dhtSensor = new DHTDigitalSensor(3, DHTDigitalSensor.VERSION.DHT22, DHTDigitalSensor.CELSIUS)
         // Digital Port 3
+        var lightSensor = new LightAnalogSensor(2)
+        // Analog Port 2
 
         console.log('GrovePi Version :: ' + board.version())
 
         // Acc. XYZ
         console.log('Acceleration I2C Sensor (single read) :: ' + accSensor.read())
-        console.log('Acceleration I2C Sensor (start stream - 1sec delay) ')
+        console.log('Acceleration I2C Sensor (start stream - 1sec delay)')
         accSensor.stream(1000, function(res) {
           console.log('Acceleration stream value=' + res)
         })
-        console.log('Acceleration I2C Sensor (start watch) ')
+        console.log('Acceleration I2C Sensor (start watch)')
         accSensor.on('change', function(res) {
           console.log('Acceleration onChange value=' + res)
         })
         accSensor.watch()
 
         // Ultrasonic Ranger
-        console.log('Ultrasonic Ranger Digital Sensor (start watch) ')
+        /*
+        console.log('Ultrasonic Ranger Digital Sensor (start watch)')
         ultrasonicSensor.on('change', function(res) {
           console.log('Ultrasonic Ranger onChange value=' + res)
         })
-        ultrasonicSensor.watch()
+        */
+        //ultrasonicSensor.watch()
 
         // Air Quality Sensor
-        console.log('AirQuality Analog Sensor (start watch) ')
+        console.log('AirQuality Analog Sensor (start watch)')
         airQualitySensor.on('change', function(res) {
           console.log('AirQuality onChange value=' + res)
         })
         airQualitySensor.watch()
 
         // DHT Sensor
-        console.log('DHT Digital Sensor (start watch) ')
+        console.log('DHT Digital Sensor (start watch)')
         dhtSensor.on('change', function(res) {
           console.log('DHT onChange value=' + res)
         })
-        dhtSensor.watch()
+        dhtSensor.watch(500) // milliseconds
+
+        // Light Sensor
+        console.log('Light Analog Sensor (start watch)')
+        lightSensor.on('change', function(res) {
+          console.log('Light onChange value=' + res)
+        })
+        lightSensor.watch()
 
         // Custom external reading
         console.log('Custom external reading')
@@ -74,7 +86,7 @@ function start() {
   board.init()
 }
 
- function customAccelerationReading() {
+function customAccelerationReading() {
   var write = board.writeBytes(Commands.acc_xyz.concat([Commands.unused, Commands.unused, Commands.unused]))
   if (write) {
     board.wait(100)
@@ -91,7 +103,7 @@ function start() {
   } else {
     return false
   }
- }
+}
 
 function onExit(err) {
   console.log('ending')
