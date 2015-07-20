1b714cd58b8037ad3673d2b0fb824f91e1139e6c
karannayan <karan25991@gmail.com>
2015-07-20 11:48:05 +0530

    Sensor examples updated for Python3
    - Oled
    - 16G accelerometer

--- Software/Python/grove_accelerometer_16g/basic_example.py
+++ Software/Python/grove_accelerometer_16g/basic_example.py
@@ -10,9 +10,9 @@ import time
 
 adxl345 = ADXL345()
     
-print "ADXL345 on address 0x%x:" % (adxl345.address)
+print ("ADXL345 on address 0x%x:" % (adxl345.address))
 while True:
 	axes = adxl345.getAxes(True)
-	print ( axes['x'] ),"\t",( axes['y'] ),"\t",( axes['z'] )
+	print (( axes['x'] ),"\t",( axes['y'] ),"\t",( axes['z'] ))
 	time.sleep(.1)
  
