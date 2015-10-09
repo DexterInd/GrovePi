1aa3b3d3b6dce3192d6e6bd74b33b0760d3c7f0c
Lee-Kevin <lidreamer@163.com>
2015-10-09 20:45:20 +0800

    To correct the mistakes of the pervious project
    pervious project can't be opened by VisualStudio 2015RTM

---
+++ Software/CSharp/GrovePi/GrovePi/Pin.cs
@@ -0,0 +1,16 @@
+﻿namespace GrovePi
+{
+    public enum Pin
+    {
+        AnalogPin0 = 0,
+        AnalogPin1 = 1,
+        AnalogPin2 = 2,
+        DigitalPin2 = 2,
+        DigitalPin3 = 3,
+        DigitalPin4 = 4,
+        DigitalPin5 = 5,
+        DigitalPin6 = 6,
+        DigitalPin7 = 7,
+        DigitalPin8 = 8
+    }
+}
