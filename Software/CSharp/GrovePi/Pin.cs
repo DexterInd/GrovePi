725f4dc32d74e7a8bc67f06be5f105389dbaa505
Exadon <Exadon@gmail.com>
2015-10-11 10:17:34 -0400

    Merge code from robsonj.
    Merge code from robsonj.
    Added ability to analog write to LED sensor

--- Software/CSharp/GrovePi/Pin.cs
+++ Software/CSharp/GrovePi/Pin.cs
@@ -3,8 +3,14 @@
     public enum Pin
     {
         AnalogPin0 = 0,
+        AnalogPin1 = 1,
+        AnalogPin2 = 2,
         DigitalPin2 = 2,
         DigitalPin3 = 3,
-        DigitalPin4 = 4
+        DigitalPin4 = 4,
+        DigitalPin5 = 5,
+        DigitalPin6 = 6,
+        DigitalPin7 = 7,
+        DigitalPin8 = 8
     }
 }
