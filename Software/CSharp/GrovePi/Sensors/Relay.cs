8b6c49d0e58365c8bd2b4e35a6f951225052b7d2
Dexter Industries <dxtrindustries@gmail.com>
2015-11-02 14:44:45 +0400

    Revert "Test submodule"

---
+++ Software/CSharp/GrovePi/Sensors/Relay.cs
@@ -0,0 +1,14 @@
+﻿namespace GrovePi.Sensors
+{
+    public interface IRelay
+    {
+        SensorStatus CurrentState { get; }
+        IRelay ChangeState(SensorStatus newState);
+    }
+    internal class Relay : Sensor<IRelay>, IRelay
+    {
+        public Relay(IGrovePi device, Pin pin) : base(device, pin, PinMode.Output)
+        {
+        }
+    }
+}
---
+++ Software/CSharp/GrovePi/Sensors/UltrasonicRangerSensor.cs
@@ -0,0 +1,28 @@
+﻿namespace GrovePi.Sensors
+{
+    public interface IUltrasonicRangerSensor
+    {
+        int MeasureInCentimeters();
+    }
+
+    internal class UltrasonicRangerSensor : IUltrasonicRangerSensor
+    {
+        private const byte CommandAddress = 7;
+        private readonly GrovePi _device;
+        private readonly Pin _pin;
+
+        internal UltrasonicRangerSensor(GrovePi device, Pin pin)
+        {
+            _device = device;
+            _pin = pin;
+        }
+
+        public int MeasureInCentimeters()
+        {
+            var buffer = new[] {CommandAddress, (byte) _pin, Constants.Unused, Constants.Unused};
+            _device.DirectAccess.Write(buffer);
+            _device.DirectAccess.Read(buffer);
+            return buffer[1]*256 + buffer[2];
+        }
+    }
+}
