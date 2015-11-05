af52e680bab8b193f48de599637c950028aedbd4
Dexter Industries <dxtrindustries@gmail.com>
2015-11-05 16:43:52 +0400

    Auto reboot on install script.

--- Script/install.sh
+++ Script/install.sh
@@ -160,3 +160,20 @@ echo " |_|  \_\______|_____/   |_/_/    \_\_|  \_\ |_|   "
 echo " "
 echo "Please restart to implement changes!"
 echo "To Restart type sudo reboot"
+
+echo "To finish changes, we will reboot the Pi."
+echo "Pi must reboot for changes and updates to take effect."
+echo "If you need to abort the reboot, press Ctrl+C.  Otherwise, reboot!"
+echo "Rebooting in 5 seconds!"
+sleep 1
+echo "Rebooting in 4 seconds!"
+sleep 1
+echo "Rebooting in 3 seconds!"
+sleep 1
+echo "Rebooting in 2 seconds!"
+sleep 1
+echo "Rebooting in 1 seconds!"
+sleep 1
+echo "Rebooting now!  Your Pi wake up with a freshly updated Raspberry Pi!"
+sleep 1
+sudo reboot
