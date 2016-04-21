4ed892a91fa003a83f495708a383de02ac5f67de
Dexter Industries <dxtrindustries@gmail.com>
2016-04-21 18:22:28 +0400

    Adding Button and Light
    Adding Button and Light Samples

---
+++ Software/CSharp/Samples/Example-Light_Sensor/StartupTask.cs
@@ -0,0 +1,73 @@
+﻿// Light Sensor Demonstration for the GrovePi.
+
+// This example combines the GrovePi and the Grove LightSensor using the Raspberry Pi and GrovePi.
+// http://www.dexterindustries.com/shop/grovepi-board/
+// http://www.dexterindustries.com/shop/grove-LightSensor/
+
+// Connct the Light Sensor to Analog Port 0 on the GrovePi.
+
+/*
+The MIT License(MIT)
+
+GrovePi for the Raspberry Pi: an open source platform for connecting Grove Sensors to the Raspberry Pi.
+Copyright (C) 2016  Dexter Industries
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+using System;
+using Windows.ApplicationModel.Background;
+
+// Add using statements to the GrovePi libraries
+using GrovePi;
+using GrovePi.Sensors;
+
+namespace LightSensor
+{
+    public sealed class StartupTask : IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            // Connect the Light Sensor to analog port 0
+            ILightSensor sensor = DeviceFactory.Build.LightSensor(Pin.AnalogPin0);
+                       
+            // Loop endlessly
+            while (true)
+            {
+                try
+                {
+                    // Check the value of the button, turn it into a string.
+                    string sensorvalue = sensor.SensorValue().ToString();
+                    System.Diagnostics.Debug.WriteLine("Sensor is " + sensorvalue);
+
+                }
+                catch (Exception ex)
+                {
+                    // NOTE: There are frequent exceptions of the following:
+                    // WinRT information: Unexpected number of bytes was transferred. Expected: '. Actual: '.
+                    // This appears to be caused by the rapid frequency of writes to the GPIO
+                    // These are being swallowed here/
+
+                    // If you want to see the exceptions uncomment the following:
+                    // System.Diagnostics.Debug.WriteLine(ex.ToString());
+                }
+            }
+        }
+    }
+}
