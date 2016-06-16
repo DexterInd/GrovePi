2a6f19079a64a33df282505dec1843605da10f9a
Karan Nayan <karan25991@gmail.com>
2016-06-16 14:31:47 +0530

    Documentation updated for the PIR sensor

--- Software/Python/grove_pir_motion_sensor.py
+++ Software/Python/grove_pir_motion_sensor.py
@@ -50,7 +50,11 @@ import time
 import grovepi
 
 # Connect the Grove PIR Motion Sensor to digital port D8
-# SIG,NC,VCC,GND
+# NOTE: Some PIR sensors come with the SIG line connected to the yellow wire and some with the SIG line connected to the white wire.
+# If the example does not work on the first run, try changing the pin number
+# For example, for port D8, if pin 8 does not work below, change it to pin 7, since each port has 2 digital pins.
+# For port 4, this would pin 3 and 4
+
 pir_sensor = 8
 motion=0
 grovepi.pinMode(pir_sensor,"INPUT")
