afa8908fd88bab1c73d6ba0b15d315835df2571c
Nicole Parrot <cleoqc1124@gmail.com>
2016-06-16 21:14:24 -0400

    Ensure GPS data is clean sometimes Latitude or Longitude are not pure floats function cleanstr ensures that no random characters can be found fucntion safefloat ensures that we can convert to a float, otherwise returns -1 added comment about which port to use on GrovePi

--- Software/Python/grove_gps/grove_gps_data.py
+++ Software/Python/grove_gps/grove_gps_data.py
@@ -39,7 +39,14 @@ THE SOFTWARE.
 # Karan      21 Aug 14 		Initial Authoring
 # Karan		 10 June 15		Updated the code to reflect the decimal GPS coordinates (contributed by rschmidt on the DI forums: http://www.dexterindustries.com/forum/?topic=gps-example-questions/#post-5668)
 # Karan		 18 Mar 16		Updated code to handle conditions where no fix from satellite
-
+#
+#
+#####################################################
+#
+# GPS SENSOR GOES INTO RPISER PORT
+#
+#####################################################
+#
 import serial, time
 import smbus
 import math
@@ -58,11 +65,25 @@ if ir_receiver_check.check_ir():
 ser = serial.Serial('/dev/ttyAMA0',  9600, timeout = 0)	#Open the serial port at 9600 baud
 ser.flush()
 
+def cleanstr(in_str):
+	out_str = "".join([c for c in in_str if c in "0123456789.-" ])
+	if len(out_str)==0:
+		out_str = "-1"
+	return out_str
+
+def safefloat(in_str):
+	try:
+		out_str = float(in_str)
+	except ValueError:
+		out_str = -1.0
+	return out_str
+
 class GPS:
 	#The GPS module used is a Grove GPS module http://www.seeedstudio.com/depot/Grove-GPS-p-959.html
 	inp=[]
 	# Refer to SIM28 NMEA spec file http://www.seeedstudio.com/wiki/images/a/a0/SIM28_DATA_File.zip
 	GGA=[]
+
 
 	#Read data from the GPS
 	def read(self):	
@@ -70,7 +91,7 @@ class GPS:
 			GPS.inp=ser.readline()
 			if GPS.inp[:6] =='$GPGGA': # GGA data , packet 1, has all the data we need
 				break
-			time.sleep(0.1)     #without the cmd program will crach
+			time.sleep(0.1)     #without the cmd program will crash
 		try:
 			ind=GPS.inp.index('$GPGGA',5,len(GPS.inp))	#Sometimes multiple GPS data packets come into the stream. Take the data only after the last '$GPGGA' is seen
 			GPS.inp=GPS.inp[ind:]
@@ -86,77 +107,86 @@ class GPS:
 			
 		time=GPS.GGA[1]
 		
-		if GPS.GGA[2]=='':
+		if GPS.GGA[2]=='':  # latitude. Technically a float
 			lat =-1.0
 		else:
-			lat=float(GPS.GGA[2])
+			lat=safefloat(cleanstr(GPS.GGA[2]))
 		
-		if GPS.GGA[3]=='':
-			lat_ns=-1.0
+		if GPS.GGA[3]=='':  # this should be either N or S
+			lat_ns=""
 		else:
-			lat_ns=float(GPS.GGA[3])
+			lat_ns=str(GPS.GGA[3])
 			
-		if  GPS.GGA[4]=='':
+		if  GPS.GGA[4]=='':  # longitude. Technically a float
 			long=-1.0
 		else:
-			long=float(GPS.GGA[4])
+			long=safefloat(cleanstr(GPS.GGA[4]))
 		
-		if  GPS.GGA[4]=='':
-			long_ew=-1.0
+		if  GPS.GGA[5]=='': # this should be either W or E
+			long_ew=""
 		else:
-			long_ew=float(GPS.GGA[5])
+			long_ew=str(GPS.GGA[5])
 			
-		fix=int(GPS.GGA[6])
-		sats=int(GPS.GGA[7])
+		fix=int(cleanstr(GPS.GGA[6]))
+		sats=int(cleanstr(GPS.GGA[7]))
 		
 		if  GPS.GGA[9]=='':
 			alt=-1.0
 		else:
-			alt=float(GPS.GGA[9])
+			# change to str instead of float
+			# 27"1 seems to be a valid value
+			alt=str(GPS.GGA[9])
 		return [time,fix,sats,alt,lat,lat_ns,long,long_ew]
 	
 	# Convert to decimal degrees
 	def decimal_degrees(self, raw_degrees):
-		degrees = float(raw_degrees) // 100
-		d = float(raw_degrees) % 100 / 60
-		return degrees + d
-		
-g=GPS()
-if enable_save_to_file:
-	f=open("gps_data.csv",'w')	#Open file to log the data
-	f.write("name,latitude,longitude\n")	#Write the header to the top of the file
-ind=0
-while True:
-	time.sleep(0.01)
-	try:
-		x=g.read()	#Read from GPS
-		[t,fix,sats,alt,lat,lat_ns,longitude,long_ew]=g.vals()	#Get the individial values
-		
-		# Convert to decimal degrees
-		if lat !=-1.0:
-			lat = g.decimal_degrees(float(lat))
-			if lat_ns == "S":
-				lat = -lat
+		try:
+			degrees = float(raw_degrees) // 100
+			d = float(raw_degrees) % 100 / 60
+			return degrees + d
+		except: 
+			return raw_degrees
 
-		if longitude !=-1.0:
-			longitude = g.decimal_degrees(float(longitude))
-			if long_ew == "W":
-				longitude = -longitude
-			
-		# print ("Time:",t,"Fix status:",fix,"Sats in view:",sats,"Altitude",alt,"Lat:",lat,lat_ns,"Long:",long,long_ew)
+
+if __name__ == "__main__":
+	g=GPS()
+	if enable_save_to_file:
+		f=open("gps_data.csv",'w')	#Open file to log the data
+		f.write("name,latitude,longitude\n")	#Write the header to the top of the file
+	ind=0
+	while True:
+		time.sleep(0.01)
+		try:
+			x=g.read()	#Read from GPS
+			[t,fix,sats,alt,lat,lat_ns,longitude,long_ew]=g.vals()	#Get the individial values
+				
+			# Convert to decimal degrees
+			if lat !=-1.0:
+				lat = g.decimal_degrees(safefloat(lat))
+				if lat_ns == "S":
+					lat = -lat
+
+			if longitude !=-1.0:
+				longitude = g.decimal_degrees(safefloat(longitude))
+				if long_ew == "W":
+					longitude = -longitude
+					
+			# print ("Time:",t,"Fix status:",fix,"Sats in view:",sats,"Altitude",alt,"Lat:",lat,lat_ns,"Long:",long,long_ew)
+			try:
+				print("Time\t\t: %s\nFix status\t: %d\nSats in view\t: %d\nAltitude\t: %s\nLat\t\t: %f\nLong\t\t: %f") %(t,fix,sats,alt,lat,longitude)
+			except:
+				print("Time\t\t: %s\nFix status\t: %s\nSats in view\t: %s\nAltitude\t: %s\nLat\t\t: %s\nLong\t\t: %s") %(t,str(fix),str(sats),str(alt),str(lat),str(longitude))
+				
+			s=str(t)+","+str(safefloat(lat)/100)+","+str(safefloat(longitude)/100)+"\n"	
+				
+			if enable_save_to_file:
+				f.write(s)	#Save to file
+			time.sleep(2)
+		except IndexError:
+			print ("Unable to read")
+		except KeyboardInterrupt:
+			if enable_save_to_file:
+				f.close()
+			print ("Exiting")
+			sys.exit(0)
 		
-		print("Time\t\t: %s\nFix status\t: %d\nSats in view\t: %d\nAltitude\t: %f\nLat\t\t: %f\nLong\t\t: %f") %(t,fix,sats,alt,lat,longitude)
-		
-		s=str(t)+","+str(float(lat)/100)+","+str(float(longitude)/100)+"\n"	
-		
-		if enable_save_to_file:
-			f.write(s)	#Save to file
-		time.sleep(2)
-	except IndexError:
-		print ("Unable to read")
-	except KeyboardInterrupt:
-		if enable_save_to_file:
-			f.close()
-		print ("Exiting")
-		sys.exit(0)
-	
