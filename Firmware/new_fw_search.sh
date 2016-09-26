d9702a38e96d0b709bee1c7e4c9961b6dad460ee
Nicole Parrot <cleoqc1124@gmail.com>
2016-09-26 19:22:57 -0400

    wget --output-document=/dev/null
    avoid orphaned index.html files

--- Firmware/new_fw_search.sh
+++ Firmware/new_fw_search.sh
@@ -6,7 +6,7 @@ read
 
 echo "Check for internet connectivity..."
 echo "=================================="
-wget -q --tries=2 --timeout=20 http://google.com
+wget -q --tries=2 --timeout=20 --output-document=/dev/null http://raspberrypi.org
 if [[ $? -eq 0 ]];then
 	echo "Connected"
 else
