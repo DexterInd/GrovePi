cd7d7f915593e3003c9920a151fd8cccb81b29ae
lanselmabor <757016515@qq.com>
2017-02-25 14:51:50 +0800

    add CSharp Driver and sample for "Grove Mini Motor Driver" and "Grove OLED Display 96_96"

---
+++ Software/CSharp/GrovePi/I2CDevices/MiniMotorDriver.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Windows.Devices.I2c;
+
+namespace GrovePi.I2CDevices
+{
+    public interface IMiniMotorDriver
+    {
+        IMiniMotorDriver drive1(int Speed);
+        IMiniMotorDriver drive2(int Speed);
+        //IMiniMotorDriver getFault();
+    }
+
+    internal sealed class MiniMotorDriver : IMiniMotorDriver
+    {
+        internal MiniMotorDriver(I2cDevice Motor1Device, I2cDevice Motor2Device)
+        {
+            if (Motor1Device == null) throw new ArgumentNullException(nameof(Motor1Device));
+            motor1DirectAccess = Motor1Device;
+
+            if (Motor2Device == null) throw new ArgumentNullException(nameof(Motor2Device));
+            motor2DirectAccess = Motor2Device;
+        }
+
+        ~MiniMotorDriver()
+        {
+            this.drive1(0);
+            this.drive2(0);
+        }
+
+        internal I2cDevice motor1DirectAccess { get; }
+        internal I2cDevice motor2DirectAccess { get; }
+
+        public IMiniMotorDriver drive1(int Speed)
+        {
+            byte regValue = 0x80;
+            motor1DirectAccess.Write(new byte[] { 0x1, regValue });
+            regValue = (byte)Math.Abs(Speed);
+            if (regValue > 63) regValue = 63;
+            regValue = (byte)(regValue * 4);
+            if (Speed < 0) regValue |= 0x01;
+            else regValue |= 0x02;
+            motor1DirectAccess.Write(new byte[] { 0x00, regValue});
+
+            return this;
+        }
+
+        public IMiniMotorDriver drive2(int Speed)
+        {
+            byte regValue = 0x80;
+            motor2DirectAccess.Write(new byte[] { 0x1, regValue });
+            regValue = (byte)Math.Abs(Speed);
+            if (regValue > 63) regValue = 63;
+            regValue = (byte)(regValue * 4);
+            if (Speed < 0) regValue |= 0x01;
+            else regValue |= 0x02;
+            motor2DirectAccess.Write(new byte[] { 0x00, regValue });
+
+            return this;
+        }
+    }
+}
