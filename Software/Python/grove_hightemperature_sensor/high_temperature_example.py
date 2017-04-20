3f36868fe483e1e461e7b628a53f74ae27ed4b21
Robert <robert.lucian.chiriac@gmail.com>
2017-04-20 19:43:19 +0300

    ref #5 : added high temperature sensor library
    * passed the tests : in boiling water and in cold water
    
    * added documentation

---
+++ Software/Python/grove_hightemperature_sensor/high_temperature_example.py
@@ -0,0 +1,45 @@
+import grove_hightemperature_sensor as grovepi # our library
+from time import sleep # and for the sleep function
+import sys # we need this for the exception throwing stuff
+
+def Main():
+    room_temperature_pin = 15 # this is equal to A1
+    probe_temperature_pin = 14 # this is equal to A0
+    # so you have to connect the sensor to A0 port
+
+    # instatiate a HighTemperatureSensor object
+    sensor = grovepi.HighTemperatureSensor(room_temperature_pin, probe_temperature_pin)
+
+    # and do this indefinitely
+    while True:
+        # read the room temperature
+        room_temperature = sensor.getRoomTemperature()
+        # and also what's important to us: the temperature at the tip of the K-Type sensor
+        probe_temperature = sensor.getTemperature()
+
+        # print it in a fashionable way
+        print('[room temperature: {:f}°C][probe temperature: {:f}°C]'.format(room_temperature, probe_temperature))
+        # and wait for 50 ms before taking another measurement - so we don't overflow the terminal
+        sleep(0.05)
+
+
+if __name__ == "__main__":
+    try:
+        Main()
+
+    # in case CTRL-C / CTRL-D keys are pressed (or anything else that might interrupt)
+    except KeyboardInterrupt:
+        print('[Keyboard interrupted]')
+        sys.exit(0)
+
+    # in case there's an IO error aka I2C
+    except IOError:
+        print('[IO Error]')
+        sys.exit(0)
+
+    # in case we have a math error (like division by 0 - can happen depending on the read values)
+    # or if the values exceed a certain threshold
+    # experiment and you'll see
+    except ValueError as e:
+        print('[{}]'.format(str(e)))
+        sys.exit(0)
