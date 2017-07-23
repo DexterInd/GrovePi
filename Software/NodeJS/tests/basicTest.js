a5302a04d0621a8168b50910c4bcd49bdc72ad1b
Dimitris-Ilias Gkanatsios <dgkanatsios@outlook.com>
2017-07-23 08:38:17 +0300

    Added DigitalButton, RotaryAngleAnalogSensor and LoudnessAnalogSensor implementations (#361)
    * Added DigitalButton, RotaryAngleAnalogSensor and LoudnessAnalogSensor implementations. Updated tests and README
    
    * Removed duplicate README.md
    
    * Re-added instructions to install Node.js

--- Software/NodeJS/tests/basicTest.js
+++ Software/NodeJS/tests/basicTest.js
@@ -6,82 +6,147 @@ var UltrasonicDigitalSensor = GrovePi.sensors.UltrasonicDigital
 var AirQualityAnalogSensor = GrovePi.sensors.AirQualityAnalog
 var DHTDigitalSensor = GrovePi.sensors.DHTDigital
 var LightAnalogSensor = GrovePi.sensors.LightAnalog
+var DigitalButtonSensor = GrovePi.sensors.DigitalButton
+var LoudnessAnalogSensor = GrovePi.sensors.LoudnessAnalog
+var RotaryAngleAnalogSensor = GrovePi.sensors.RotaryAnalog
 
 var board
+
+
+var testOptions = {
+  acceleration: false,
+  ultrasonic: false,
+  airQuality: false,
+  dhtDigital: false,
+  lightAnalog: false,
+  digitalButton: false,
+  loudnessAnalog: false,
+  rotaryAngle: true,
+  customAccelerationReading: false
+}
 
 function start() {
   console.log('starting')
 
   board = new Board({
     debug: true,
-    onError: function(err) {
+    onError: function (err) {
       console.log('TEST ERROR')
       console.log(err)
     },
-    onInit: function(res) {
+    onInit: function (res) {
       if (res) {
-        var accSensor = new AccelerationI2cSensor()
-        // I2C sensor doesn't need to specify a port
-        var ultrasonicSensor = new UltrasonicDigitalSensor(4)
-        // Digital Port 4
-        var airQualitySensor = new AirQualityAnalogSensor(1)
-        // Analog Port 1
-        var dhtSensor = new DHTDigitalSensor(3, DHTDigitalSensor.VERSION.DHT22, DHTDigitalSensor.CELSIUS)
-        // Digital Port 3
-        var lightSensor = new LightAnalogSensor(2)
-        // Analog Port 2
 
         console.log('GrovePi Version :: ' + board.version())
 
-        // Acc. XYZ
-        console.log('Acceleration I2C Sensor (single read) :: ' + accSensor.read())
-        console.log('Acceleration I2C Sensor (start stream - 1sec delay)')
-        accSensor.stream(1000, function(res) {
-          console.log('Acceleration stream value=' + res)
-        })
-        console.log('Acceleration I2C Sensor (start watch)')
-        accSensor.on('change', function(res) {
-          console.log('Acceleration onChange value=' + res)
-        })
-        accSensor.watch()
+        if (testOptions.acceleration) {
+          var accSensor = new AccelerationI2cSensor()
+          // I2C sensor doesn't need to specify a port
+          // Acc. XYZ
+          console.log('Acceleration I2C Sensor (single read) :: ' + accSensor.read())
+          console.log('Acceleration I2C Sensor (start stream - 1sec delay)')
+          accSensor.stream(1000, function (res) {
+            console.log('Acceleration stream value=' + res)
+          })
+          console.log('Acceleration I2C Sensor (start watch)')
+          accSensor.on('change', function (res) {
+            console.log('Acceleration onChange value=' + res)
+          })
+          accSensor.watch()
+        }
 
-        // Ultrasonic Ranger
-        console.log('Ultrasonic Ranger Digital Sensor (start watch)')
-        ultrasonicSensor.on('change', function(res) {
-          console.log('Ultrasonic Ranger onChange value=' + res)
-        })
-        //ultrasonicSensor.watch()
+        if (testOptions.ultrasonic) {
+          var ultrasonicSensor = new UltrasonicDigitalSensor(3)
+          // Digital Port 3
+          // Ultrasonic Ranger
+          console.log('Ultrasonic Ranger Digital Sensor (start watch)')
+          ultrasonicSensor.on('change', function (res) {
+            console.log('Ultrasonic Ranger onChange value=' + res)
+          })
+          ultrasonicSensor.watch()
+        }
 
-        // Air Quality Sensor
-        console.log('AirQuality Analog Sensor (start watch)')
-        airQualitySensor.on('change', function(res) {
-          console.log('AirQuality onChange value=' + res)
-        })
-        airQualitySensor.watch()
+        if (testOptions.airQuality) {
+          var airQualitySensor = new AirQualityAnalogSensor(1)
+          // Analog Port 1
+          // Air Quality Sensor
+          console.log('AirQuality Analog Sensor (start watch)')
+          airQualitySensor.on('change', function (res) {
+            console.log('AirQuality onChange value=' + res)
+          })
+          airQualitySensor.watch()
+        }
 
-        // DHT Sensor
-        console.log('DHT Digital Sensor (start watch)')
-        dhtSensor.on('change', function(res) {
-          console.log('DHT onChange value=' + res)
-        })
-        dhtSensor.watch(500) // milliseconds
+        if (testOptions.dhtDigital) {
+          var dhtSensor = new DHTDigitalSensor(3, DHTDigitalSensor.VERSION.DHT22, DHTDigitalSensor.CELSIUS)
+          // Digital Port 3
+          // DHT Sensor
+          console.log('DHT Digital Sensor (start watch)')
+          dhtSensor.on('change', function (res) {
+            console.log('DHT onChange value=' + res)
+          })
+          dhtSensor.watch(500) // milliseconds
+        }
 
-        // Light Sensor
-        console.log('Light Analog Sensor (start watch)')
-        lightSensor.on('change', function(res) {
-          console.log('Light onChange value=' + res)
-        })
-        lightSensor.watch()
+        if (testOptions.lightAnalog) {
+          var lightSensor = new LightAnalogSensor(3)
+          // Analog Port 3
+          // Light Sensor
+          console.log('Light Analog Sensor (start watch)')
+          lightSensor.on('change', function (res) {
+            console.log('Light onChange value=' + res)
+          })
+          lightSensor.watch()
+        }
 
-        // Custom external reading
-        console.log('Custom external reading')
-        console.log('customAccelerationReading()::' + customAccelerationReading())
+        if (testOptions.loudnessAnalog) {
+          var loudnessSensor = new LoudnessAnalogSensor(2)
+          //Analog Port 2
+          // Loudness Sensor
+          console.log('Loudness Analog Sensor (start monitoring - reporting results every 10s)')
+          loudnessSensor.start()
+          setInterval(loudnessSensorGetAvgMax, 10000, loudnessSensor)
+        }
+
+        if (testOptions.digitalButton) {
+          var buttonSensor = new DigitalButtonSensor(4)
+          //Digital Port 4
+          // Button sensor
+          console.log('Digital Button Sensor (start watch)')
+          buttonSensor.on('down', function (res) {
+            //res will be either singlepress or longpress
+            console.log('Button onDown, data=' + res)
+          })
+          button.watch()
+        }
+
+        if (testOptions.rotaryAngle) {
+          var rotaryAngleSensor = new RotaryAngleAnalogSensor(1)
+          //Analog Port 1
+          // Rotary Angle Sensor
+          console.log('Rotary Angle Sensor (start watch)')
+          rotaryAngleSensor.start()
+          rotaryAngleSensor.on('data', function (res) {
+            console.log('Rotary onData value=' + res)
+          })
+        }
+
+        if (testOptions.customAccelerationReading) {
+          // Custom external reading
+          console.log('Custom external reading')
+          console.log('customAccelerationReading()::' + customAccelerationReading())
+        }
       } else {
         console.log('TEST CANNOT START')
       }
     }
   })
   board.init()
+}
+
+function loudnessSensorGetAvgMax(loudnessSensor) {
+  var res = loudnessSensor.readAvgMax()
+  console.log('Loudness avg value=' + res.avg + ' and max value=' + res.max)
 }
 
 function customAccelerationReading() {
@@ -91,9 +156,9 @@ function customAccelerationReading() {
     board.readByte()
     var bytes = board.readBytes()
     if (bytes instanceof Buffer) {
-      var x = bytes[1] > 32 ? -(bytes[1]-224) : bytes[1]
-      var y = bytes[2] > 32 ? -(bytes[2]-224) : bytes[2]
-      var z = bytes[3] > 32 ? -(bytes[3]-224) : bytes[3]
+      var x = bytes[1] > 32 ? -(bytes[1] - 224) : bytes[1]
+      var y = bytes[2] > 32 ? -(bytes[2] - 224) : bytes[2]
+      var z = bytes[3] > 32 ? -(bytes[3] - 224) : bytes[3]
       return [x, y, z]
     } else {
       return false
