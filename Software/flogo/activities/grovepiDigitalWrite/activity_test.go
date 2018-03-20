a30e54cee855cfccc29a0a7898d85631b0fdf0bf
Jörg Grote <joerg@grote-online.de>
2018-03-20 18:19:26 +0100

    first draft

---
+++ Software/flogo/activities/grovepiDigitalWrite/activity_test.go
@@ -0,0 +1,62 @@
+package grovepi
+
+import (
+	"testing"
+
+	"io/ioutil"
+
+	"github.com/TIBCOSoftware/flogo-lib/flow/activity"
+	"github.com/TIBCOSoftware/flogo-lib/flow/test"
+)
+
+var activityMetadata *activity.Metadata
+
+func getActivityMetadata() *activity.Metadata {
+
+	if activityMetadata == nil {
+		jsonMetadataBytes, err := ioutil.ReadFile("activity.json")
+		if err != nil {
+			panic("No Json Metadata found for activity.json path")
+		}
+
+		activityMetadata = activity.NewMetadata(string(jsonMetadataBytes))
+	}
+
+	return activityMetadata
+}
+
+func TestCreate(t *testing.T) {
+
+	act := NewActivity(getActivityMetadata())
+
+	if act == nil {
+		t.Error("Activity Not Created")
+		t.Fail()
+		return
+	}
+}
+
+func TestGrovePiDW(t *testing.T) {
+
+	defer func() {
+		if r := recover(); r != nil {
+			t.Failed()
+			t.Errorf("panic during execution: %v", r)
+		}
+	}()
+
+	act := NewActivity(getActivityMetadata())
+	tc := test.NewTestActivityContext(getActivityMetadata())
+
+	//setup attrs
+	tc.SetInput(ivPin, 3)
+	tc.SetInput(ivValue, true)
+
+	act.Eval(tc)
+
+	value := tc.GetOutput(ovSuccess).(bool)
+
+	if value != 1 {
+		t.Fail()
+	}
+}
