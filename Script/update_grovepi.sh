4565d292483e1d1b594f24ef230afa28439dcbc6
Robert Lucian CHIRIAC <robert.lucian.chiriac@gmail.com>
2018-04-24 18:09:17 +0300

    feature - add feedback output and cut script_tools' output

--- Script/update_grovepi.sh
+++ Script/update_grovepi.sh
@@ -52,47 +52,6 @@ for i; do
   esac
 done
 
-# in case the following packages are not installed and `--no-dependencies` option has been used
-if [[ $installdependencies = "false" ]]; then
-  command -v git >/dev/null 2>&1 || { echo "I require git but it's not installed. Don't use --no-dependencies option. Aborting." >&2; exit 1; }
-  command -v python >/dev/null 2>&1 || { echo "Executable \"python\" couldn't be found. Don't use --no-dependencies option. Aborting." >&2; exit 2; }
-  command -v python3 >/dev/null 2>&1 || { echo "Executable \"python3\" couldn't be found. Don't use --no-dependencies option. Aborting." >&2; exit 3; }
-  command -v pip >/dev/null 2>&1 || { echo "Executable \"pip\" couldn't be found. Don't use --no-dependencies option. Aborting." >&2; exit 4; }
-  command -v pip3 >/dev/null 2>&1 || { echo "Executable \"pip3\" couldn't be found. Don't use --no-dependencies option. Aborting." >&2; exit 5; }
-fi
-
-# create rest of list of arguments for script_tools call
-optionslist+=("$selectedbranch")
-optionslist+=("--install-python-package")
-[[ $usepython3exec = "true" ]] && optionslist+=("--use-python3-exe-too")
-[[ $updaterepo = "true" ]] && optionslist+=("--update-aptget")
-[[ $installdependencies = "true" ]] && optionslist+=("--install-deb-deps")
-[[ $install_pkg_scriptools = "true" ]] && optionslist+=("--install-python-package")
-
-# update script_tools first
-# to be replaced with `dexterindustries.com/update_tools` when it's all ready
-curl -kL https://raw.githubusercontent.com/RobertLucian/script_tools/feature/arg-based-installation/install_script_tools.sh > $PIHOME/tmp_script_tools.sh
-bash $PIHOME/tmp_script_tools.sh ${optionslist[@]}
-rm $PIHOME/tmp_script_tools.sh
-
-# HAVE TO UNCOMMENT ONCE check_internet makes it to script_tools
-# check if there's internet access,
-# otherwise straight out exit the script
-# check_internet
-
-# needs to be sourced from here when we call this as a standalone
-source /home/pi/$DEXTER/lib/$DEXTER/script_tools/functions_library.sh
-
-# create folders recursively if they don't exist already
-mkdir -p $DEXTER_PATH
-cd $DEXTER_PATH
-
-# it's simpler and more reliable (for now) to just delete the repo and clone a new one
-# otherwise, we'd have to deal with all the intricacies of git
-sudo rm -rf $GROVEPI_DIR
-git clone --quiet --depth=1 -b $selectedbranch https://github.com/DexterInd/GrovePi.git
-cd $GROVEPI_DIR
-
 # show some feedback for the GrovePi
 if ! quiet_mode
   echo "  _____            _                                ";
@@ -115,7 +74,58 @@ if ! quiet_mode
   echo "| |__| | | | (_) \ V /  __/ |   | |  "
   echo " \_____|_|  \___/ \_/ \___|_|   |_|  "
   feedback "Welcome to GrovePi Installer."
+  echo " "
 fi
+
+echo "Updating GrovePi for $selectedbranch branch with the following options:"
+echo "--no-dependencies=$installdependencies"
+([[ $updaterepo = "true" ]] && echo "--no-update-aptget=false") || echo "--no-update-aptget=true"
+echo "--no-update-aptget=$updaterepo"
+echo "--bypass-pkg-scriptools=$install_pkg_scriptools"
+echo "--user-local=$userlocal"
+echo "--env-local=$envlocal"
+echo "--system-wide=$systemwide"
+
+# in case the following packages are not installed and `--no-dependencies` option has been used
+if [[ $installdependencies = "false" ]]; then
+  command -v git >/dev/null 2>&1 || { echo "I require git but it's not installed. Don't use --no-dependencies option. Aborting." >&2; exit 1; }
+  command -v python >/dev/null 2>&1 || { echo "Executable \"python\" couldn't be found. Don't use --no-dependencies option. Aborting." >&2; exit 2; }
+  command -v python3 >/dev/null 2>&1 || { echo "Executable \"python3\" couldn't be found. Don't use --no-dependencies option. Aborting." >&2; exit 3; }
+  command -v pip >/dev/null 2>&1 || { echo "Executable \"pip\" couldn't be found. Don't use --no-dependencies option. Aborting." >&2; exit 4; }
+  command -v pip3 >/dev/null 2>&1 || { echo "Executable \"pip3\" couldn't be found. Don't use --no-dependencies option. Aborting." >&2; exit 5; }
+fi
+
+# create rest of list of arguments for script_tools call
+optionslist+=("$selectedbranch")
+optionslist+=("--install-python-package")
+[[ $usepython3exec = "true" ]] && optionslist+=("--use-python3-exe-too")
+[[ $updaterepo = "true" ]] && optionslist+=("--update-aptget")
+[[ $installdependencies = "true" ]] && optionslist+=("--install-deb-deps")
+[[ $install_pkg_scriptools = "true" ]] && optionslist+=("--install-python-package")
+
+# update script_tools first
+# to be replaced with `dexterindustries.com/update_tools` when it's all ready
+curl -kL https://raw.githubusercontent.com/RobertLucian/script_tools/feature/arg-based-installation/install_script_tools.sh > $PIHOME/tmp_script_tools.sh
+bash $PIHOME/tmp_script_tools.sh ${optionslist[@]} > /dev/null
+rm $PIHOME/tmp_script_tools.sh
+
+# HAVE TO UNCOMMENT ONCE check_internet makes it to script_tools
+# check if there's internet access,
+# otherwise straight out exit the script
+# check_internet
+
+# needs to be sourced from here when we call this as a standalone
+source /home/pi/$DEXTER/lib/$DEXTER/script_tools/functions_library.sh
+
+# create folders recursively if they don't exist already
+mkdir -p $DEXTER_PATH
+cd $DEXTER_PATH
+
+# it's simpler and more reliable (for now) to just delete the repo and clone a new one
+# otherwise, we'd have to deal with all the intricacies of git
+sudo rm -rf $GROVEPI_DIR
+git clone --quiet --depth=1 -b $selectedbranch https://github.com/DexterInd/GrovePi.git
+cd $GROVEPI_DIR
 
 # installing dependencies
 pushd $GROVEPI_DIR/Script > /dev/null
