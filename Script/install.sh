b1a481bebb789b7d3d6aaf36d3e8cbe2634ad7d7
Robert Lucian Chiriac <robert.lucian.chiriac@gmail.com>
2018-05-09 16:02:30 +0300

    feature - make grovepi easy to install (#397)
    * feature - use setup.py to install the package & trim-down scripts
    
    * feature - more bulk changes to the GrovePi installer
    
    * feature - change script_tools url for testing purposes
    
    * feature - fix runner for script_tools and uncomment a call
    
    * feature - remove calls that are no longer needed
    
    * feature - more granular control over the installation phase
    
    * feature - organize install commands and remove unneeded install script
    
    * feature - add in another option for install script
    
    * feature - fix syntax error caused by bad alignment
    
    * feature - add in descriptive comment
    
    * feature - add feedback output and cut script_tools' output
    
    * feature - fixing syntax error on if statement
    
    * feature - update install README
    
    * feature - update the root README
    
    * feature - missing comma at the end of a sentence
    
    * feature - fix console feedback issue
    
    * feature  - clarification on the install README
    
    * feature - show logo each time
    
    * feature - fix parser issue with script_tools
    
    * feature - use feedback function if available
    
    * feature - minor but important changes
    
    * feature - removing duplicate code
    
    * feature - fixing some output stuff on curling and echos
    
    * feature - missing extension on script and restoring real url address
    
    * feature - make some output formatting and add a comment to README
    
    * feature - bad call on pip command (pip2 instead of pip)
    
    * feature - add a few missing packages with apt-get
    
    * feature - exit the install script if git is not installed in all cases
    
    * feature - make path more readable
    
    * feature - revert a check for git presence
    
    * feature - exit install script if script_tools fails
    
    * feature - don't build packages and instead, rely on apt-get
    
    * feature - add python package remover + some comments
    
    * feature - make sure the Dexter folder can be created
    
    * feature - put everything in functions
    
    * feature - remove wiringpi installation
    
    * feature - add comment on a needed basis
    
    * feature - use 3rd person instead of 1st person
    
    * feature - add nodejs in
    
    * feature - use quiet mode on the GrovePi text-logo
    
    * feature - pass arguments to function in install script
    
    * feature - removing some echoes and moving a variable
    
    * feature - removing the last empty echoes
    
    * feature - remove some unnecessary and ugly echoes and adding more details
    
    * feature - add an echo in here and remove another one
    
    * feature - fix syntax error on if statement
    
    * feature - replacing "aborting" with "exiting"
    
    * feature - check if script is run with pi user
    
    * feature - correct name of installer
    
    * feature - fix global variable issue
    
    * feature - fix how curl command gets piped to bash

--- Script/install.sh
+++ Script/install.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-curl --silent https://raw.githubusercontent.com/DexterInd/script_tools/master/install_script_tools.sh | bash
 
 PIHOME=/home/pi
 DEXTERSCRIPT=$PIHOME/Dexter/lib/Dexter/script_tools
@@ -11,74 +10,25 @@ REPO_PATH=$(readlink -f $(dirname $0) | grep -E -o "^(.*?\\GrovePi)")
 
 source $DEXTERSCRIPT/functions_library.sh
 
-identify_cie() {
-    if ! quiet_mode
-    then
-        echo "  _____            _                                ";
-        echo " |  __ \          | |                               ";
-        echo " | |  | | _____  _| |_ ___ _ __                     ";
-        echo " | |  | |/ _ \ \/ / __/ _ \ '__|                    ";
-        echo " | |__| |  __/>  <| ||  __/ |                       ";
-        echo " |_____/ \___/_/\_\\\__\___|_|          _            ";
-        echo " |_   _|         | |         | |      (_)           ";
-        echo "   | |  _ __   __| |_   _ ___| |_ _ __ _  ___  ___  ";
-        echo "   | | | '_ \ / _\ | | | / __| __| '__| |/ _ \/ __| ";
-        echo "  _| |_| | | | (_| | |_| \__ \ |_| |  | |  __/\__ \ ";
-        echo " |_____|_| |_|\__,_|\__,_|___/\__|_|  |_|\___||___/ ";
-        echo "                                                    ";
-        echo "                                                    ";
-        echo " "
-    fi
-}
-
-identify_robot() {
-	echo "  _____                    _____ _ "
-	echo " / ____|                  |  __ (_)  "
-	echo "| |  __ _ __ _____   _____| |__) |   "
-	echo "| | |_ | '__/ _ \ \ / / _ \  ___/ |  "
-	echo "| |__| | | | (_) \ V /  __/ |   | |  "
-	echo " \_____|_|  \___/ \_/ \___|_|   |_|  "
-
-	feedback "Welcome to GrovePi Installer."
-}
-
 display_welcome_msg() {
-	echo " "
-	echo "Requirements:"
-	echo "1) Must be connected to the internet"
-	echo "2) This script must be run as root user"
-	echo " "
-	echo "Steps:"
-	echo "1) Installs package dependencies:"
-	echo "   - python-pip       alternative Python package installer"
-	echo "   - git              fast, scalable, distributed revision control system"
-	echo "   - libi2c-dev       userspace I2C programming library development files"
-	echo "   - python-serial    pyserial - module encapsulating access for the serial port"
-	echo "   - python-rpi.gpio  Python GPIO module for Raspberry Pi"
-	echo "   - i2c-tools        This Python module allows SMBus access through the I2C /dev"
-	echo "   - python-smbus     Python bindings for Linux SMBus access through i2c-dev"
-	echo "   - python3-smbus    Python3 bindings for Linux SMBus access through i2c-dev"
-	echo "   - arduino          AVR development board IDE and built-in libraries"
-	echo "   - minicom          friendly menu driven serial communication program"
-  echo "   - numpy            is the fundamental package for scientific computing with Python"
-	echo "2) Clone, build wiringPi in GrovePi/Script and install it"
-	echo "3) Removes I2C and SPI from modprobe blacklist /etc/modprobe.d/raspi-blacklist.conf"
-	echo "4) Adds I2C-dev, i2c-bcm2708 and spi-dev to /etc/modules"
-	echo "5) Installs gertboard avrdude_5.10-4_armhf.deb package"
-	echo "6) Runs gertboard setup"
-	echo "   - configures avrdude"
-	echo "   - downloads gertboard known boards and programmers"
-	echo "   - replaces avrsetup with gertboards version"
-	echo "   - in /etc/inittab comments out lines containing AMA0"
-	echo "   - in /boot/cmdline.txt removes: console=ttyAMA0,115200 kgdboc=ttyAMA0,115200 console=tty1"
-	echo "   - in /usr/share/arduino/hardware/arduino creates backup of boards.txt"
-	echo "   - in /usr/share/arduino/hardware/arduino creates backup of programmers.txt"
-	echo " "
 	echo "Special thanks to Joe Sanford at Tufts University. This script was derived from his work. Thank you Joe!"
-	echo " "
-	echo "Raspberry Pi wil reboot after completion."
-	echo " "
-	echo " "
+}
+
+install_dependencies() {
+    # the sudo apt-get update is already
+    # done by the script_tools installer in
+    # update_grovepi.sh
+
+  	feedback "Installing dependencies for the GrovePi"
+    # in order for nodejs to be installed, the repo for it
+    # needs to be in; this is all done in script_tools while doing an apt-get update
+    sudo apt-get install nodejs -y
+
+  	sudo apt-get install git libi2c-dev i2c-tools arduino minicom -y
+    sudo apt-get install python-pip python-smbus python-dev python-serial python-rpi.gpio python-scipy python-numpy -y
+    sudo apt-get install python3-pip python3-smbus python3-dev python3-rpi.gpio python3-rpi.gpio python3-scipy python3-numpy -y
+
+    feedback "Dependencies for the GrovePi installed"
 }
 
 check_root_user() {
@@ -86,71 +36,13 @@ check_root_user() {
         feedback "FAIL!  This script must be run as such: sudo ./install.sh"
         exit 1
     fi
-    echo " "
-}
-
-check_internet() {
-    if ! quiet_mode ; then
-        feedback "Check for internet connectivity..."
-        feedback "=================================="
-        wget -q --tries=2 --timeout=20 --output-document=/dev/null https://raspberrypi.org
-        if [ $? -eq 0 ];then
-            echo "Connected to the Internet"
-        else
-            echo "Unable to Connect, try again !!!"
-            exit 0
-        fi
-    fi
-}
-
-install_dependencies() {
-    if ! quiet_mode ; then
-        sudo apt-get update
-    fi
-    echo " "
-	feedback "Installing Dependencies"
-	echo "======================="
-	sudo apt-get install python-pip git libi2c-dev python-serial i2c-tools python-smbus python3-smbus arduino minicom python-dev -y
-	sudo apt-get purge python-rpi.gpio -y
-	sudo apt-get purge python3-rpi.gpio -y
-	sudo apt-get install python-rpi.gpio -y
-	sudo apt-get install python3-rpi.gpio -y
-       sudo apt-get install python-scipy -y
-       sudo apt-get install python3-scipy -y
-	sudo pip install -U RPi.GPIO
-  sudo pip2 install numpy
-  sudo pip3 install numpy
-
-    feedback "Dependencies installed"
-}
-
-install_wiringpi() {
-    # Check if WiringPi Installed
-
-    # using curl piped to bash does not leave a file behind. no need to remove it
-    # we can do either the curl - it works just fine
-    # sudo curl https://raw.githubusercontent.com/DexterInd/script_tools/master/update_wiringpi.sh | bash
-    # or call the version that's already on the SD card
-    sudo bash $DEXTERSCRIPT/update_wiringpi.sh
-    # done with WiringPi
-
-    # remove wiringPi directory if present
-    if [ -d wiringPi ]
-    then
-        sudo rm -r wiringPi
-    fi
-    # End check if WiringPi installed
-    echo " "
 }
 
 install_spi_i2c() {
-	echo " "
 	RASPI_BL="/etc/modprobe.d/raspi-blacklist.conf.bak"
 	MODS="i2c spi"
 	if [ -f ${RASPI_BL} ]; then
 		feedback "Removing blacklist from ${RASPI_BL} . . ."
-		feedback "=================================================================="
-		echo " "
 		for i in ${MODS}
 		do
 			MOD_NAME=$(echo $i | tr [a-z] [A-Z])
@@ -160,9 +52,7 @@ install_spi_i2c() {
 	fi
 
 	#Adding in /etc/modules
-	feedback " "
 	feedback "Adding I2C-dev and SPI-dev in /etc/modules . . ."
-	feedback "================================================"
 	if grep -q "i2c-dev" /etc/modules; then
 		echo "I2C-dev already present"
 	else
@@ -182,9 +72,7 @@ install_spi_i2c() {
 		echo "spi-dev added"
 	fi
 
-	echo " "
 	feedback "Making I2C changes in /boot/config.txt . . ."
-	feedback "================================================"
 
 	BOOT_CONFIG="/boot/config.txt"
 	DTPARAMS="i2c1 i2c_arm"
@@ -202,76 +90,16 @@ install_spi_i2c() {
 }
 
 install_avr() {
-	feedback "Installing avrdude"
-	feedback "=================="
-	source /home/pi/Dexter/lib/Dexter/script_tools/install_avrdude.sh
+	feedback "Installing avrdude for the GrovePi"
+	source $DEXTERSCRIPT/install_avrdude.sh
 	create_avrdude_folder
-    install_avrdude
-    cd $ROBOT_DIR
-    echo "done with AVRDUDE "
+  install_avrdude
+  cd $ROBOT_DIR
+  echo "done with AVRDUDE for the GrovePi"
 }
 
-install_python_libs(){
-	feedback "Install python libraries"
-	echo "If you see errors related to /etc/inittab, it's fine."
-	echo "/etc/inittab has been deprecated in favor of systemd,"
-	echo "cfr. https://www.raspberrypi.org/forums/viewtopic.php?f=66&t=123081"
-
-	echo " "
-
-	sudo apt-get install python-smbus -y
-
-	echo " "
-	echo "Making libraries global . . ."
-	echo "============================="
-	if [ -d /usr/lib/python2.7/dist-packages ]; then
-		# Usually "/" used as delimiter in sed commands but since REPO_PATH variable contains many "/" and sed command gets
-		# confused on expanding REPO_PATH as it finds "/" to be delimiter, hence here "@" is used as delimiter
-		sudo sed -i "s@^/Software@$REPO_PATH&@" $REPO_PATH/Script/grove.pth
-		sudo cp ${DIR_PATH}/Script/grove.pth /usr/lib/python2.7/dist-packages/grove.pth
-	else
-		echo "/usr/lib/python2.7/dist-packages not found, exiting"
-		exit 1
-	fi
-	if [ -d /usr/lib/python3/dist-packages ]; then
-		# Usually "/" used as delimiter in sed commands but since REPO_PATH variable contains many "/" and sed command gets
-		# confused on expanding REPO_PATH as it finds "/" to be delimiter, hence here "@" is used as delimiter
-		sudo sed -i "s@^/Software@$REPO_PATH&@" $REPO_PATH/Script/grove.pth
-		sudo cp ${DIR_PATH}/Script/grove.pth /usr/lib/python3/dist-packages/grove.pth
-	else
-		echo "/usr/lib/python3/dist-packages not found, exiting"
-		exit 1
-	fi
-	echo "Done"
-}
-
-call_for_reboot() {
-    if ! quiet_mode ; then
-        feedback " "
-        feedback "Please restart the Raspberry Pi for the changes to take effect"
-        feedback " "
-        feedback "Please restart to implement changes!"
-        feedback "  _____  ______  _____ _______       _____ _______ "
-        feedback " |  __ \|  ____|/ ____|__   __|/\   |  __ \__   __|"
-        feedback " | |__) | |__  | (___    | |  /  \  | |__) | | |   "
-        feedback " |  _  /|  __|  \___ \   | | / /\ \ |  _  /  | |   "
-        feedback " | | \ \| |____ ____) |  | |/ ____ \| | \ \  | |   "
-        feedback " |_|  \_\______|_____/   |_/_/    \_\_|  \_\ |_|   "
-        feedback " "
-        feedback "Please restart to implement changes!"
-        feedback "To Restart type sudo reboot"
-    fi
-}
-
-identify_cie
-identify_robot
 display_welcome_msg
-# sleep 5
-check_internet
 check_root_user
 install_dependencies
-install_wiringpi
 install_spi_i2c
 install_avr
-install_python_libs
-call_for_reboot
--- Script/update_grovepi.sh
+++ Script/update_grovepi.sh
@@ -1,38 +1,251 @@
+#! /bin/bash
 # This script updates the the code repos on Raspbian for Robots.
+
+################################################
+######## Parsing Command Line Arguments ########
+################################################
 
 # definitions needed for standalone call
 PIHOME=/home/pi
 DEXTER=Dexter
 DEXTER_PATH=$PIHOME/$DEXTER
 RASPBIAN=$PIHOME/di_update/Raspbian_For_Robots
-curl --silent https://raw.githubusercontent.com/DexterInd/script_tools/master/install_script_tools.sh | bash
-
-# needs to be sourced from here when we call this as a standalone
-source /home/pi/$DEXTER/lib/$DEXTER/script_tools/functions_library.sh
-
 GROVEPI_DIR=$DEXTER_PATH/GrovePi
+DEXTERSCRIPT=$DEXTER_PATH/lib/Dexter/script_tools
 
-# Check for a GrovePi directory under "Dexter" folder.  If it doesn't exist, create it.
+# the top-level module name of grovepi package
+# used for detecting whether it's installed or not
+REPO_PACKAGE=grovepi
 
-if [ -d "$GROVEPI_DIR" ]; then
-    echo "GrovePi Directory Exists"
-    cd $GROVEPI_DIR             # Go to directory
-    sudo git fetch origin       # Hard reset the git files
-    sudo git reset --hard  
-    sudo git merge origin/master
-else
-	echo "Cloning"
-    cd $PIHOME/$DEXTER/
-    git clone https://github.com/DexterInd/GrovePi
-    cd GrovePi
-fi
+# called way down bellow
+check_if_run_with_pi() {
+  ## if not running with the pi user then exit
+  if [ $(id -ur) -ne $(id -ur pi) ]; then
+    echo "GrovePi installer script must be run with \"pi\" user. Exiting."
+    exit 7
+  fi
+}
 
-change_branch $BRANCH   # Change to a branch we're working on in the GrovePi Directory. 
-                        # Variable $BRANCH comes from /upd_script/fetch.sh
+# called way down below
+parse_cmdline_arguments() {
 
-feedback "--> Start GrovePi update install."
-feedback "---------------------------------"
-pushd $PIHOME/$DEXTER/GrovePi/Script > /dev/null
-sudo chmod +x install.sh
-sudo bash ./install.sh
-popd > /dev/null
+  # whether to install the dependencies or not (avrdude, apt-get, wiringpi, and so on)
+  installdependencies=true
+  updaterepo=true
+  install_pkg_scriptools=true
+
+  # the following 3 options are mutually exclusive
+  systemwide=true
+  userlocal=false
+  envlocal=false
+  usepython3exec=true
+
+  # the following option tells which branch has to be used
+  selectedbranch="master"
+
+  declare -ga optionslist=("--system-wide")
+  # iterate through bash arguments
+  for i; do
+    case "$i" in
+      --no-dependencies)
+        installdependencies=false
+        ;;
+      --no-update-aptget)
+        updaterepo=false
+        ;;
+      --bypass-pkg-scriptools)
+        install_pkg_scriptools=false
+        ;;
+      --user-local)
+        userlocal=true
+        systemwide=false
+        declare -ga optionslist=("--user-local")
+        ;;
+      --env-local)
+        envlocal=true
+        systemwide=false
+        declare -ga optionslist=("--env-local")
+        ;;
+      --system-wide)
+        ;;
+      develop|feature/*|hotfix/*|fix/*|DexterOS*|v*)
+        selectedbranch="$i"
+        ;;
+    esac
+  done
+
+  # show some feedback on the console
+  if [ -f $DEXTERSCRIPT/functions_library.sh ]; then
+    source $DEXTERSCRIPT/functions_library.sh
+    # show some feedback for the GrovePi
+    if [ ! quiet_mode ]; then
+      echo "  _____            _                                ";
+      echo " |  __ \          | |                               ";
+      echo " | |  | | _____  _| |_ ___ _ __                     ";
+      echo " | |  | |/ _ \ \/ / __/ _ \ '__|                    ";
+      echo " | |__| |  __/>  <| ||  __/ |                       ";
+      echo " |_____/ \___/_/\_\\\__\___|_|          _            ";
+      echo " |_   _|         | |         | |      (_)           ";
+      echo "   | |  _ __   __| |_   _ ___| |_ _ __ _  ___  ___  ";
+      echo "   | | | '_ \ / _\ | | | / __| __| '__| |/ _ \/ __| ";
+      echo "  _| |_| | | | (_| | |_| \__ \ |_| |  | |  __/\__ \ ";
+      echo " |_____|_| |_|\__,_|\__,_|___/\__|_|  |_|\___||___/ ";
+      echo "                                                    ";
+      echo "                                                    ";
+      echo "  _____                    _____ _ "
+      echo " / ____|                  |  __ (_)  "
+      echo "| |  __ _ __ _____   _____| |__) |   "
+      echo "| | |_ | '__/ _ \ \ / / _ \  ___/ |  "
+      echo "| |__| | | | (_) \ V /  __/ |   | |  "
+      echo " \_____|_|  \___/ \_/ \___|_|   |_|  "
+      echo " "
+    fi
+    feedback "Welcome to GrovePi Installer."
+  else
+    echo "Welcome to GrovePi Installer."
+  fi
+  echo "Updating GrovePi for $selectedbranch branch with the following options:"
+  ([[ $installdependencies = "true" ]] && echo "  --no-dependencies=false") || echo "  --no-dependencies=true"
+  ([[ $updaterepo = "true" ]] && echo "  --no-update-aptget=false") || echo "  --no-update-aptget=true"
+  ([[ $install_pkg_scriptools = "true" ]] && echo "  --bypass-pkg-scriptools=false") || echo "  --bypass-pkg-scriptools=true"
+  echo "  --user-local=$userlocal"
+  echo "  --env-local=$envlocal"
+  echo "  --system-wide=$systemwide"
+
+  # in case the following packages are not installed and `--no-dependencies` option has been used
+  if [[ $installdependencies = "false" ]]; then
+    command -v git >/dev/null 2>&1 || { echo "This script requires \"git\" but it's not installed. Don't use --no-dependencies option. Exiting." >&2; exit 1; }
+    command -v python >/dev/null 2>&1 || { echo "Executable \"python\" couldn't be found. Don't use --no-dependencies option. Exiting." >&2; exit 2; }
+    command -v python3 >/dev/null 2>&1 || { echo "Executable \"python3\" couldn't be found. Don't use --no-dependencies option. Exiting." >&2; exit 3; }
+    command -v pip >/dev/null 2>&1 || { echo "Executable \"pip\" couldn't be found. Don't use --no-dependencies option. Exiting." >&2; exit 4; }
+    command -v pip3 >/dev/null 2>&1 || { echo "Executable \"pip3\" couldn't be found. Don't use --no-dependencies option. Exiting." >&2; exit 5; }
+  fi
+
+  # create rest of list of arguments for script_tools call
+  optionslist+=("$selectedbranch")
+  [[ $usepython3exec = "true" ]] && optionslist+=("--use-python3-exe-too")
+  [[ $updaterepo = "true" ]] && optionslist+=("--update-aptget")
+  [[ $installdependencies = "true" ]] && optionslist+=("--install-deb-deps")
+  [[ $install_pkg_scriptools = "true" ]] && optionslist+=("--install-python-package")
+
+  echo "Options used for script_tools script: \"${optionslist[@]}\""
+}
+
+################################################
+######## Cloning GrovePi & Script_Tools  #######
+################################################
+
+# called way down below
+clone_grovepi_and_scriptools() {
+  # update script_tools first
+  curl --silent -kL dexterindustries.com/update_tools > $PIHOME/.tmp_script_tools.sh
+  echo "Installing script_tools. This might take a while.."
+  bash $PIHOME/.tmp_script_tools.sh ${optionslist[@]} > /dev/null
+  ret_val=$?
+  rm $PIHOME/.tmp_script_tools.sh
+  if [[ $ret_val -ne 0 ]]; then
+    echo "script_tools failed installing with exit code $ret_val. Exiting."
+    exit 6
+  fi
+  echo "Done installing script_tools"
+
+  # needs to be sourced from here when we call this as a standalone
+  source $DEXTERSCRIPT/functions_library.sh
+
+  # create folders recursively if they don't exist already
+  # we use sudo for creating the dir(s) because on older versions of R4R
+  # the sudo command is used, and hence we need to be sure we have write permissions.
+  sudo mkdir -p $DEXTER_PATH
+  # still only available for the pi user
+  # shortly after this, we'll make it work for any user
+  sudo chown pi:pi -R $DEXTER_PATH
+  cd $DEXTER_PATH
+
+  # it's simpler and more reliable (for now) to just delete the repo and clone a new one
+  # otherwise, we'd have to deal with all the intricacies of git
+  sudo rm -rf $GROVEPI_DIR
+  git clone --quiet --depth=1 -b $selectedbranch https://github.com/DexterInd/GrovePi.git
+  cd $GROVEPI_DIR
+}
+
+################################################
+######## Install Python Packages & Deps ########
+################################################
+
+# called by <<install_python_pkgs_and_dependencies>>
+install_python_packages() {
+  [[ $systemwide = "true" ]] && sudo python setup.py install \
+              && [[ $usepython3exec = "true" ]] && sudo python3 setup.py install
+  [[ $userlocal = "true" ]] && python setup.py install --user \
+              && [[ $usepython3exec = "true" ]] && python3 setup.py install --user
+  [[ $envlocal = "true" ]] && python setup.py install \
+              && [[ $usepython3exec = "true" ]] && python3 setup.py install
+}
+
+# called by <<install_python_pkgs_and_dependencies>>
+remove_python_packages() {
+  # the 1st and only argument
+  # takes the name of the package that needs to removed
+  rm -f $PIHOME/.pypaths
+
+  # get absolute path to python package
+  # saves output to file because we want to have the syntax highlight working
+  # does this for both root and the current user because packages can be either system-wide or local
+  # later on the strings used with the python command can be put in just one string that gets used repeatedly
+  python -c "import pkgutil; import os; \
+              eggs_loader = pkgutil.find_loader('$1'); found = eggs_loader is not None; \
+              output = os.path.dirname(os.path.realpath(eggs_loader.get_filename('$1'))) if found else ''; print(output);" >> $PIHOME/.pypaths
+  sudo python -c "import pkgutil; import os; \
+              eggs_loader = pkgutil.find_loader('$1'); found = eggs_loader is not None; \
+              output = os.path.dirname(os.path.realpath(eggs_loader.get_filename('$1'))) if found else ''; print(output);" >> $PIHOME/.pypaths
+  if [[ $usepython3exec = "true" ]]; then
+    python3 -c "import pkgutil; import os; \
+                eggs_loader = pkgutil.find_loader('$1'); found = eggs_loader is not None; \
+                output = os.path.dirname(os.path.realpath(eggs_loader.get_filename('$1'))) if found else ''; print(output);" >> $PIHOME/.pypaths
+    sudo python3 -c "import pkgutil; import os; \
+                eggs_loader = pkgutil.find_loader('$1'); found = eggs_loader is not None; \
+                output = os.path.dirname(os.path.realpath(eggs_loader.get_filename('$1'))) if found else ''; print(output);" >> $PIHOME/.pypaths
+  fi
+
+  # removing eggs for $1 python package
+  # ideally, easy-install.pth needs to be adjusted too
+  # but pip seems to know how to handle missing packages, which is okay
+  while read path;
+  do
+    if [ ! -z "${path}" -a "${path}" != " " ]; then
+      echo "Removing ${path} egg"
+      sudo rm -f "${path}"
+    fi
+  done < $PIHOME/.pypaths
+}
+
+# called way down bellow
+install_python_pkgs_and_dependencies() {
+  # installing dependencies if required
+  if [[ $installdependencies = "true" ]]; then
+    feedback "Installing GrovePi dependencies. This might take a while.."
+    pushd $GROVEPI_DIR/Script > /dev/null
+    sudo bash ./install.sh
+    popd > /dev/null
+  fi
+
+  # feedback "Removing \"$REPO_PACKAGE\" and \"$DHT_PACKAGE\" to make space for new ones"
+  feedback "Removing \"$REPO_PACKAGE\" to make space for the new one"
+  remove_python_packages "$REPO_PACKAGE"
+  # remove_python_packages "$DHT_PACKAGE"
+
+  # installing the package itself
+  pushd $GROVEPI_DIR/Software/Python > /dev/null
+  install_python_packages
+  popd > /dev/null
+}
+
+################################################
+######## Aggregating all function calls ########
+################################################
+
+check_if_run_with_pi
+parse_cmdline_arguments "$@"
+clone_grovepi_and_scriptools
+install_python_pkgs_and_dependencies
+exit 0
