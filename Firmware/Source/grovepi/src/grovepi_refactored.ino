7a5d98f82d25a252bb44efb56463636b0c355d63
Robert Lucian CHIRIAC <robert.lucian.chiriac@gmail.com>
2018-07-03 18:15:28 +0300

    fix - i2c transfer issues with rpi

--- Firmware/Source/grovepi/src/grovepi_refactored.ino
+++ Firmware/Source/grovepi/src/grovepi_refactored.ino
@@ -14,13 +14,24 @@ struct Request{
   uint8_t command_id;
   uint8_t no_bytes;
   uint8_t payload[MAX_PAYLOAD];
+
+  Request& operator=(const Request &d)
+  {
+    this->command_id = d.command_id;
+    this->no_bytes = d.no_bytes;
+    for(uint8_t i = 0; i < this->no_bytes; i++)
+      this->payload[i] = d.payload[i];
+
+    return *this;
+  }
 };
 
-Queue<Request> requests = Queue<Request>(15);
-Request new_request;
+Request request;
 Request temp_request;
 uint8_t receive_counter = 0;
 uint8_t main_counter = 0;
+bool busy_loop = false;
+bool new_request = false;
 
 void flushI2C();
 
@@ -35,9 +46,11 @@ void setup()
 
 void loop()
 {
-  if(requests.count() > 0)
+  if(new_request == true)
   {
-    temp_request = requests.pop();
+    busy_loop = true;
+
+    temp_request = request;
     Serial.print("CID=" + String(temp_request.command_id) + "+Bytes=" + String(temp_request.no_bytes) + "+List=");
     main_counter = 0;
     while(main_counter < temp_request.no_bytes)
@@ -46,35 +59,38 @@ void loop()
       main_counter++;
     }
     Serial.println();
+
+    new_request = false;
+    busy_loop = false;
   }
 }
 
 void receiveData(int byteCount)
 {
-  if(byteCount >= 2)
-  {
-    new_request.command_id = Wire.read();
-    new_request.no_bytes = Wire.read();
-    byteCount -= 2;
-  }
+  // discard any incoming data if the main loop is busy
+  if(busy_loop == true) flushI2C();
   else
   {
-    flushI2C();
-    return;
-  }
+    if(byteCount >= 2)
+    {
+      request.command_id = Wire.read();
+      request.no_bytes = Wire.read();
+      byteCount -= 2;
 
-  if(byteCount == new_request.no_bytes)
-  {
-    receive_counter = 0;
-    while(receive_counter < byteCount)
-      new_request.payload[receive_counter++] = Wire.read();
+      if(byteCount == request.no_bytes)
+      {
+        receive_counter = 0;
+        while(receive_counter < byteCount)
+        {
+          request.payload[receive_counter] = Wire.read();
+          receive_counter++;
+        }
+        new_request = true;
+      }
+      else flushI2C();
+    }
+    else flushI2C();
   }
-  else{
-    flushI2C();
-    return;
-  }
-
-  requests.push(new_request);
 }
 
 void sendData()
@@ -84,6 +100,6 @@ void sendData()
 
 void flushI2C()
 {
-  while(Wire.available() > 0)
+  while(Wire.available())
     Wire.read();
 }
