2cd161d27616bebd2c19df186e21bdc7f2793379
Robert Lucian CHIRIAC <robert.lucian.chiriac@gmail.com>
2018-08-14 19:35:02 +0300

    Revert "hotfix - add temporary links to other branches"
    This reverts commit 465170560deae384adf6a3c4fa3fd2ce6fb42aac.

--- Script/update_grovepi.sh
+++ Script/update_grovepi.sh
@@ -161,8 +161,7 @@ install_rfrtools_repo() {
 
   # if rfrtools is not bypassed then install it
   if [[ $install_rfrtools = "true" ]]; then
-    # curl --silent -kL https://raw.githubusercontent.com/DexterInd/RFR_Tools/$selectedbranch/scripts/install_tools.sh > $PIHOME/.tmp_rfrtools.sh
-    curl --silent -kL https://raw.githubusercontent.com/RobertLucian/RFR_Tools/hotfix/add-gui-bypass/scripts/install_tools.sh > $PIHOME/.tmp_rfrtools.sh
+    curl --silent -kL https://raw.githubusercontent.com/DexterInd/RFR_Tools/$selectedbranch/scripts/install_tools.sh > $PIHOME/.tmp_rfrtools.sh
     echo "Installing RFR_Tools. This might take a while.."
     bash $PIHOME/.tmp_rfrtools.sh ${rfrtools_options[@]} # > /dev/null
     ret_val=$?
@@ -190,8 +189,7 @@ clone_grovepi() {
   # it's simpler and more reliable (for now) to just delete the repo and clone a new one
   # otherwise, we'd have to deal with all the intricacies of git
   sudo rm -rf $GROVEPI_DIR
-  # git clone --quiet --depth=1 -b $selectedbranch https://github.com/DexterInd/GrovePi.git
-  git clone --quiet --depth=1 -b hotfix/add-minimal-install https://github.com/RobertLucian/GrovePi.git
+  git clone --quiet --depth=1 -b $selectedbranch https://github.com/DexterInd/GrovePi.git
   cd $GROVEPI_DIR
 }
 
