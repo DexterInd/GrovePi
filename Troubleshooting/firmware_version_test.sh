503aa6376b0ad146bcb32416d0aadad6172d28b9
CleoQc <cleoqc1124@gmail.com>
2019-10-24 15:47:42 -0400

    cannot run firmware version check with sudo

--- Troubleshooting/firmware_version_test.sh
+++ Troubleshooting/firmware_version_test.sh
@@ -3,4 +3,4 @@ REPO_PATH=$(readlink -f $(dirname $0) | grep -E -o "^(.*?\\GrovePi)")
 echo ""
 echo Checking for firmware version
 echo =============================
-sudo python $REPO_PATH/Software/Python/grove_firmware_version_check.py
+python $REPO_PATH/Software/Python/grove_firmware_version_check.py
