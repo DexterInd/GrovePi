e5dcd07a407960344e102b13a4ebc9ee45710389
Mimry <mimry@protonmail.com>
2020-01-09 19:52:02 +0100

    Added files for the addresses 8 to 12. Updated to multi_installer.sh script accordingly.

--- Script/multi_grovepi_installer/multi_installer.sh
+++ Script/multi_grovepi_installer/multi_installer.sh
@@ -1,78 +1,20 @@
 #!/usr/bin/env sh
-while true ; do
-    echo "What address would you like to use the GrovePi on?"
-	echo "Addr:"
-    echo " 3"
-    echo " 4 (Default)"
-    echo " 5"
-    echo " 6"
-    echo " 7"
-    echo "Any other key to exit"
+echo "What address would you like to use the GrovePi on?"
+read ADDRESS
 
-    read INPUT
-    if [ $INPUT -eq 3 ] ; then
-        echo "Setting up GrovePi with Address 3"
-        echo "..."
-		echo "BURNING FIRMWARE"
-		sudo avrdude -c gpio -p m328p -U flash:w:grove_pi_v1_2_2_addr3.cpp.hex
-		echo "INSTALLING PYTHON LIBRARY"
-		sudo python setup3.py install
-		echo "I2C DEVICES AVAILABLE"
-		sleep 1
-		sudo i2cdetect -y 1
-		exit 0
-		
-    elif [ $INPUT -eq 4 ] ; then
-        echo "Setting up GrovePi with Address 4"
-        echo "..."
-		echo "BURNING FIRMWARE"
-		sudo avrdude -c gpio -p m328p -U flash:w:grove_pi_v1_2_2_addr4.cpp.hex
-		echo "INSTALLING PYTHON LIBRARY"
-		sudo python setup4.py install
-		echo "I2C DEVICES AVAILABLE"
-		sleep 1
-		sudo i2cdetect -y 1
-		
-		exit 0
-		
-    elif [ $INPUT -eq 5 ] ; then
-        echo "Setting up GrovePi with Address 5"
-        echo "..."
-		echo "BURNING FIRMWARE"
-		sudo avrdude -c gpio -p m328p -U flash:w:grove_pi_v1_2_2_addr5.cpp.hex
-		echo "INSTALLING PYTHON LIBRARY"
-		sudo python setup5.py install
-		echo "I2C DEVICES AVAILABLE"
-		sleep 1
-		sudo i2cdetect -y 1
-		exit 0
-		
-    elif [ $INPUT -eq 6 ] ; then
-        echo "Setting up GrovePi with Address 6"
-        echo "..."
-		echo "BURNING FIRMWARE"
-		sudo avrdude -c gpio -p m328p -U flash:w:grove_pi_v1_2_2_addr6.cpp.hex
-		echo "INSTALLING PYTHON LIBRARY"
-		sudo python setup6.py install
-		echo "I2C DEVICES AVAILABLE"
-		sleep 1
-		sudo i2cdetect -y 1
-		exit 0
-		
-    elif [ $INPUT -eq 7 ] ; then
-        echo "Setting up GrovePi with Address 7"
-        echo "..."
-		echo "BURNING FIRMWARE"
-		sudo avrdude -c gpio -p m328p -U flash:w:grove_pi_v1_2_2_addr7.cpp.hex
-		echo "INSTALLING PYTHON LIBRARY"
-		sudo python setup7.py install
-		echo "I2C DEVICES AVAILABLE"
-		sleep 1
-		sudo i2cdetect -y 1
-		exit 0
-		
-    else
-        echo "invalid choice"
-		exit 0
-    fi
-done
+# Check if the neccessary files for the address exist.
+if [ -e "grove_pi_v1_2_2_addr$ADDRESS.cpp.hex" ] && [ -e "setup$ADDRESS.py" ] && [ -e "grovepi$ADDRESS.py" ]; then
+  echo "Setting up GrovePi with Address $ADDRESS"
+  echo "BURNING FIRMWARE"
+  echo "..."
+  sudo avrdude -c gpio -p m328p -U flash:w:grove_pi_v1_2_2_addr"$ADDRESS".cpp.hex
+  echo "INSTALLING PYTHON LIBRARY"
+  sudo python setup"$ADDRESS".py install
+  echo "I2C DEVICES AVAILABLE"
+  sleep 1
+  sudo i2cdetect -y 1
+  exit 0
+else
+  echo "The neccessary files for this address do not exist."
+  exit 0
+fi
